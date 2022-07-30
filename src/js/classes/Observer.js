2cdd861de12fb48f2649239d9b68d3f50e184910
unknown <coderawii@gmail.com>
2022-07-30 20:49:05 +0100

    fix: issue w removePreloaderAnimation() and addRule

--- src/js/classes/Observer.js
+++ src/js/classes/Observer.js
@@ -1,104 +1,103 @@
-const $from_bottom = document.querySelectorAll(".anim--fromBottom")
-const $from_top = document.querySelectorAll(".anim--fromTop")
-const $from_left = document.querySelectorAll(".anim--fromLeft")
-const $from_right = document.querySelectorAll(".anim--fromRight")
+const $from_bottom = document.querySelectorAll(".anim--fromBottom");
+const $from_top = document.querySelectorAll(".anim--fromTop");
+const $from_left = document.querySelectorAll(".anim--fromLeft");
+const $from_right = document.querySelectorAll(".anim--fromRight");
 // const $imgs = [...document.querySelectorAll(".anim--imgs")]
-const $imgs = document.querySelectorAll(".anim--imgs")
-const $circle_imgs = document.querySelectorAll(".anim--circle-imgs")
-const $icons = document.querySelectorAll(".anim--icon")
-const $from_bottom_cards = document.querySelectorAll(".anim--card-fromBottom")
-const $scale = document.querySelectorAll(".anim--scale")
-const $fade_in = document.querySelectorAll(".anim--fadeIn")
-const $from_right_rotating = document.querySelector(".anim--fromRight-rotating")
+const $imgs = document.querySelectorAll(".anim--imgs");
+const $circle_imgs = document.querySelectorAll(".anim--circle-imgs");
+const $icons = document.querySelectorAll(".anim--icon");
+const $from_bottom_cards = document.querySelectorAll(".anim--card-fromBottom");
+const $scale = document.querySelectorAll(".anim--scale");
+const $fade_in = document.querySelectorAll(".anim--fadeIn");
+const $from_right_rotating = document.querySelector(
+  ".anim--fromRight-rotating"
+);
 
 export default class Observer {
-	anim_observer() {
-		const appearOptions = {
-			threshold: 1,
-			rootMargin: "-300px 0px 0px 0px",
-		}
-		const appearOnScroll = new IntersectionObserver((entries, observer) => {
-			entries.forEach((entry) => {
-				if (!entry.isIntersecting) {
-					return
-				} else {
-					entry.target.classList.add("appear")
-					appearOnScroll.unobserve(entry.target) //! stop looking on something when u've done ur job
-				}
-			}, appearOptions)
-		})
+  anim_observer() {
+    const appearOptions = {
+      threshold: 1,
+      rootMargin: "-300px 0px 0px 0px",
+    };
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add("appear");
+          appearOnScroll.unobserve(entry.target); //! stop looking on something when u've done ur job
+        }
+      }, appearOptions);
+    });
 
-		//* primeniti observer na niz elemenata
-		appearOnScroll.observe($from_right_rotating)
+    //* primeniti observer na niz elemenata
+    appearOnScroll.observe($from_right_rotating);
 
-		$from_bottom.forEach((bottom) => {
-			appearOnScroll.observe(bottom)
-		})
-		$from_bottom_cards.forEach((card) => {
-			appearOnScroll.observe(card)
-		})
-		$from_top.forEach((top) => {
-			appearOnScroll.observe(top)
-		})
-		$from_left.forEach((left) => {
-			appearOnScroll.observe(left)
-		})
-		$from_right.forEach((right) => {
-			appearOnScroll.observe(right)
-		})
-		$icons.forEach((icon) => {
-			appearOnScroll.observe(icon)
-		})
-		$imgs.forEach((img) => {
-			appearOnScroll.observe(img)
+    $from_bottom.forEach((bottom) => {
+      appearOnScroll.observe(bottom);
+    });
+    $from_bottom_cards.forEach((card) => {
+      appearOnScroll.observe(card);
+    });
+    $from_top.forEach((top) => {
+      appearOnScroll.observe(top);
+    });
+    $from_left.forEach((left) => {
+      appearOnScroll.observe(left);
+    });
+    $from_right.forEach((right) => {
+      appearOnScroll.observe(right);
+    });
+    $icons.forEach((icon) => {
+      appearOnScroll.observe(icon);
+    });
+    $imgs.forEach((img) => {
+      appearOnScroll.observe(img);
+    });
+    $circle_imgs.forEach((circle_img) => {
+      appearOnScroll.observe(circle_img);
+    });
+    $scale.forEach((scale) => {
+      appearOnScroll.observe(scale);
+    });
+    $fade_in.forEach((fade) => {
+      appearOnScroll.observe(fade);
+    });
+  }
 
-			console.log(img)
-		})
-		$circle_imgs.forEach((circle_img) => {
-			appearOnScroll.observe(circle_img)
-		})
-		$scale.forEach((scale) => {
-			appearOnScroll.observe(scale)
-		})
-		$fade_in.forEach((fade) => {
-			appearOnScroll.observe(fade)
-		})
-	}
-
-	browserDontSupportObserver() {
-		this.removeAnimClassFromArr($from_bottom)
-		this.removeAnimClassFromArr($from_top)
-		this.removeAnimClassFromArr($from_left)
-		this.removeAnimClassFromArr($from_right)
-		this.removeAnimClassFromArr($imgs)
-		this.removeAnimClassFromArr($circle_imgs)
-		this.removeAnimClassFromArr($icons)
-		this.removeAnimClassFromArr($from_bottom_cards)
-		this.removeAnimClassFromArr($scale)
-		this.removeAnimClassFromArr($fade_in)
-		this.removeAnimClassFromIndividualEl($from_right_rotating)
-		this.addTransitionOnSmallCircleImgs()
-	}
-	removeAnimClassFromArr(_arr) {
-		const prefix = "anim--"
-		_arr.forEach((el) => {
-			const classes = el.className
-				.split(" ")
-				.filter((c) => !c.startsWith(prefix))
-			el.className = classes.join(" ").trim()
-		})
-	}
-	removeAnimClassFromIndividualEl(_oneEl) {
-		const prefix = "anim--"
-		const classes = _oneEl.className
-			.split(" ")
-			.filter((c) => !c.startsWith(prefix))
-		_oneEl.className = classes.join(" ").trim()
-	}
-	addTransitionOnSmallCircleImgs() {
-		$circle_imgs.forEach((img) => {
-			img.style.transition = "1s ease-in-out"
-			console.log(img.style.transition)
-		})
-	}
+  browserDontSupportObserver() {
+    this.removeAnimClassFromArr($from_bottom);
+    this.removeAnimClassFromArr($from_top);
+    this.removeAnimClassFromArr($from_left);
+    this.removeAnimClassFromArr($from_right);
+    this.removeAnimClassFromArr($imgs);
+    this.removeAnimClassFromArr($circle_imgs);
+    this.removeAnimClassFromArr($icons);
+    this.removeAnimClassFromArr($from_bottom_cards);
+    this.removeAnimClassFromArr($scale);
+    this.removeAnimClassFromArr($fade_in);
+    this.removeAnimClassFromIndividualEl($from_right_rotating);
+    this.addTransitionOnSmallCircleImgs();
+  }
+  removeAnimClassFromArr(_arr) {
+    const prefix = "anim--";
+    _arr.forEach((el) => {
+      const classes = el.className
+        .split(" ")
+        .filter((c) => !c.startsWith(prefix));
+      el.className = classes.join(" ").trim();
+    });
+  }
+  removeAnimClassFromIndividualEl(_oneEl) {
+    const prefix = "anim--";
+    const classes = _oneEl.className
+      .split(" ")
+      .filter((c) => !c.startsWith(prefix));
+    _oneEl.className = classes.join(" ").trim();
+  }
+  addTransitionOnSmallCircleImgs() {
+    $circle_imgs.forEach((img) => {
+      img.style.transition = "1s ease-in-out";
+    });
+  }
 }
--- src/js/classes/UI.js
+++ src/js/classes/UI.js
@@ -1,35 +1,33 @@
 export default class UI {
-	displayContent() {
-		document.querySelector('.content').style.display = 'block'
-	}
+  displayContent() {
+    document.querySelector(".content").style.display = "block";
+  }
 
-	hidePreloader() {
-		document.querySelector('.preloader').classList.add('preloader--hidden')
-	}
+  hidePreloader() {
+    document.querySelector(".preloader").classList.add("preloader--hidden");
+  }
 
-	hideAfterloader() {
-		document.querySelector('.afterloader').classList.add('afterloader--hidden')
-	}
+  hideAfterloader() {
+    document.querySelector(".afterloader").classList.add("afterloader--hidden");
+  }
 
-	addAnimationClass() {
-		const body = document.body || document.documentElement.body
-		if (body.classList.contains('animate-all ')) return
-		body.classList.add('animate-all')
-	}
+  addAnimationClass() {
+    const body = document.body || document.documentElement.body;
+    if (body.classList.contains("animate-all ")) return;
+    body.classList.add("animate-all");
+  }
 
-	removePreloaderAnimation() {
-		document.querySelector('.preloader__circle').style.animation = 'none';
-		document.styleSheets[0].addRule('.preloader__circle::before', 'animation: none');
-		document.styleSheets[0].addRule('.preloader__circle::after', 'animation: none');
-		// console.log(document.querySelector('.preloader__circle').style.animation);
+  removePreloaderAnimation() {
+    document
+      .querySelector(".preloader__circle")
+      .classList.add(".preloader--animation-none");
 
-		// this.pseudoElementsCss()
-	}
+    // console.log(document.styleSheets);
+    // document.querySelector(".preloader__circle").style.animation = "none";
+    // document.styleSheets[0].addRule('.preloader__circle::before', 'animation: none');
+    // document.styleSheets[0].addRule('.preloader__circle::after', 'animation: none');
+    // console.log(document.querySelector('.preloader__circle').style.animation);
 
-	// pseudoElementsCss() {
-	// 	const beforeValue = window.getComputedStyle(document.querySelector('.preloader__circle'), '::before').getPropertyValue('animation');
-	// 	const afterValue = window.getComputedStyle(document.querySelector('.preloader__circle'), '::after').getPropertyValue('animation');
-
-	// 	return console.log(`beforeValue: ${beforeValue} and afterValue: ${afterValue}`)
-	// }
+    // this.pseudoElementsCss()
+  }
 }
